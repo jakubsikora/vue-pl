3a730f2bd966616adb7a865a4cfc76f08a95acb6
Jakub Sikora <jakub.sikora.en@gmail.com>
2017-02-09 20:03:40 +0000

    Grid wip

---
+++ src/components/grid/Grid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="pl-grid" :class="{ 'pl-grid__gutters': gutters, 'pl-grid__flex-cells': flexCells }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      gutters: {
+        type: Boolean,
+        default: true,
+      },
+      flexCells: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+</script>
+
+<style lang="scss">
+  @import '../../components/base.scss';
+
+  .pl-grid {
+    display: flex;
+
+    .pl-grid__cell {
+      flex: 1 1 0%;
+    }
+  }
+
+  .pl-grid__gutters {
+    > .pl-grid__cell {
+      padding: 1rem 0 0 1rem;
+    }
+  }
+
+  .pl-grid__flex-cells {
+    > .pl-grid__cell {
+      display: flex;
+    }
+  }
+</style>
--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -4,38 +4,69 @@
 
     <div class="sg-example__box">
       <h4>Basic Grid</h4>
-      <div class="pl-grid">
-        <div class="pl-grid__cell">
-          <div class="sg-example__box-item">Fit / Full</div>
-        </div>
-
-        <div class="pl-grid__cell">
-          <div class="sg-example__box-item">Fit / Full</div>
-        </div>
-      </div>
-    </div>
-
-    <div class="sg-example__box">
-      <h4>Basic Grid</h4>
       <Grid>
-        <div class="pl-grid__cell">
-          <div class="sg-example__box-item">Fit / Full</div>
-        </div>
+        <GridCell>
+          <div class="sg-example__box-item">1/2</div>
+        </GridCell>
 
-        <div class="pl-grid__cell">
-          <div class="sg-example__box-item">Fit / Full</div>
-        </div>
+        <GridCell>
+          <div class="sg-example__box-item">1/2</div>
+        </GridCell>
+      </Grid>
+
+      <Grid>
+        <GridCell>
+          <div class="sg-example__box-item">1/3</div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">1/3</div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">1/3</div>
+        </GridCell>
+      </Grid>
+
+      <Grid>
+        <GridCell>
+          <div class="sg-example__box-item">1/4</div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">1/4</div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">1/4</div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">1/4</div>
+        </GridCell>
+      </Grid>
+
+      <Grid flex-cells>
+        <GridCell>
+          <div class="sg-example__box-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+        </GridCell>
+
+        <GridCell>
+          <div class="sg-example__box-item">Cras porttitor varius nibh, quis feugiat dui faucibus at. Morbi sollicitudin nulla nec leo bibendum, ut pretium ipsum dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst. Aenean scelerisque urna in consectetur gravida. Curabitur consectetur venenatis elementum. Ut a efficitur leo.</div>
+        </GridCell>
       </Grid>
     </div>
   </div>
 </template>
 
 <script>
-  import Grid from '../components/Grid';
+  import Grid from '../components/grid/Grid';
+  import GridCell from '../components/grid/GridCell';
 
   export default {
     components: {
       Grid,
+      GridCell,
     },
   };
 </script>
@@ -43,15 +74,8 @@
 <style lang="scss">
   @import '../components/base.scss';
 
-  .pl-grid {
-    display: flex;
-
-    .pl-grid__cell {
-      flex: 1 1 auto;
-    }
-  }
-
   .sg-example__box-item {
+    width: 100%;
     background-color: $color-light;
     padding: .5rem;
     text-align: center;
