297bfedc39e126b6cc39e17cc4e77255f394ffdc
Jakub Sikora <jakub.sikora@pure360.com>
2017-02-10 17:29:01 +0000

    update paths

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import foundationsRoutes from './foundations';
 Vue.use(Router);
 
 const router = new Router({
+  base: '/vue-pl/',
   mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: [
