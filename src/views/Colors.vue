ac43de70a7e744db39a7112da03baad4bbf14d7e
Jakub Sikora <jakub.sikora@pure360.com>
2017-02-20 15:41:06 +0000

    wip

--- src/views/Colors.vue
+++ src/views/Colors.vue
@@ -3,13 +3,22 @@
     <h2>Colors</h2>
 
     <div class="sg-example__box">
-      <h4>Text color</h4>
+      <h4>Text color: $color-text</h4>
 
-      <div class="sg-example__box-item sg-example__box-item--text">$color-text: #F2F5F7</div>
+      <div class="sg-example__box-item sg-example__box-item--text">&nbsp;</div>
+
+      <h4>Background color: $color-bg</h4>
+      <div class="sg-example__box-item sg-example__box-item--bg">&nbsp;</div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data() {
+      return {
+        bgColor: '#2A3F54',
+      };
+    },
+  };
 </script>
